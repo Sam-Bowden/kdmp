04dbf9ec19a2bb5193a142343ff794936460b901
Sam Bowden <samuel.bowden1@protonmail.com>
2022-02-14 12:09:04 +0000

    Add basis for client and server with test communication

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,3 +1,71 @@
+use gtk4::prelude::*;
+use gtk4::{Application, ApplicationWindow, Entry};
+use gtk4::glib::{self, Continue, MainContext, PRIORITY_DEFAULT, clone};
+use std::os::unix::net::UnixStream;
+use std::thread;
+use std::io::Write;
+
 fn main() {
-    println!("Hello, world!");
+    let app = Application::builder()
+        .build();
+
+    app.connect_activate(build_ui);
+
+    app.run();
+}
+
+fn build_ui(app: &Application) {
+    let entry = Entry::builder()
+        .margin_top(12)
+        .margin_bottom(12)
+        .margin_start(12)
+        .margin_end(12)
+        .build();
+
+    let (sender, receiver) = MainContext::channel(PRIORITY_DEFAULT);
+
+    entry.connect_activate(move |_| {
+        let sender = sender.clone();
+        thread::spawn(move || {
+            let msg = match send_request() {
+                Ok(_) => "ok",
+                Err(e) => e,
+            };
+
+            sender.send(msg).expect("Error sending result through channel")
+        });
+    });
+    
+    receiver.attach(None, clone!(@weak app => @default-return Continue(false), move |msg| {
+        if msg == "ok" {
+            app.quit();
+        } else {
+            println!("Error: {}", msg);
+        }
+
+        glib::Continue(true)
+    }),);
+
+    let window = ApplicationWindow::builder()
+        .application(app)
+        .title("KDMP")
+        .child(&entry)
+        .default_height(100)
+        .default_width(300)
+        .resizable(false)
+        .build();
+
+    window.present();
+}
+
+fn send_request() -> Result<(), &'static str> {
+    let mut stream = match UnixStream::connect("/tmp/kdmp.sock") {
+        Ok(s) => s,
+        Err(_) => return Err("Failed to connect to daemon"),
+    };
+
+    match stream.write_all("test".as_bytes()) {
+        Ok(()) => Ok(()),
+        Err(_) => Err("Failed to communicate to daemon"),
+    }
 }
--- daemon/src/main.rs
+++ daemon/src/main.rs
@@ -1,3 +1,20 @@
+use std::os::unix::net::UnixListener;
+use std::path::Path;
+use std::fs;
+use std::io::Read;
+
 fn main() {
-    println!("Hello, world!");
+    let socket = Path::new("/tmp/kdmp.sock");
+
+    if socket.exists() {
+        fs::remove_file(&socket).expect("Error deleting existing socket");
+    }
+
+    let listener = UnixListener::bind(&socket).expect("Error connecting to socket");
+
+    for stream in listener.incoming() {
+        let mut response = String::new();
+        stream.unwrap().read_to_string(&mut response).unwrap();
+        println!("{}", response);
+    }
 }
